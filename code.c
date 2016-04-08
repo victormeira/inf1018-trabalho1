eb7de276a75140cea06f0431ee90eec71df76121
victormeira <victormeirapinto@icloud.com>
2016-04-08 13:35:22 -0300

    implementacao da decodificao de byte e simplificacao

--- code.c
+++ code.c
@@ -3,6 +3,7 @@
 
 
 #include "code.h"
+#include <math.h>
 #define BYTE 8
 
 int code (char* desc, void* v, FILE* f)
@@ -10,51 +11,8 @@ int code (char* desc, void* v, FILE* f)
 	/*esta funcao codifica o blablabla*/
 }
 
-/* Funcao 
-int fgetc(FILE *arq) retorna como int do primeiro byte do arquivo*/
-
-void one_field(FILE *f)
-{
-	int i, num;
-
-	for(i=0;i<6;i++)
-		c=fgetc(f);							/*passa os 5 bits 0 da chave */
-
-	if(fgetc(f)==0)							/*confere se é long ou int e imprime */
-		printf("<int>");
-	else
-		printf("<long>");
-
-	while(fgetc(f)==1)						/*confere se é ultimo byte */
-	{						
-		for(i=0;i<BYTE-1;i++,mult++)
-		{
-				/*implementar sabendo que inteiro tem que ser transformado com sinal */
-		}
-	}
-
-	for(i=0;i<BYTE-1;i++,mult++) 			/* Processa ultimo byte */
-	{
-				/*implementar sabendo que inteiro tem que ser transformado com sinal */
-	}
-
-	printf("%d\n",num);
-
-	return;
-}
-
-void one_struct(FILE *f)
-{
-	int i, c, exp=0, num=0;
-
-	for(i=0;i<BYTE-1;i++)
-		c=fgetc(f);								/* acaba de ler a marca FF da estrutura */
-
-	while(fgetc(f)==1)							/* confere se é o ultimo campo */
-		one_field(f);
-
-	one_field(f);								/* processa ultimo campo */
-}
+int one_field(FILE *f);
+int one_struct(FILE *c);
 
 int decode (FILE *f)
 {
@@ -66,7 +24,7 @@ int decode (FILE *f)
 		return -1;
 	}
 
-	while(fgetc(f)==1)							/*	enquanto ainda é puxado bit valor 1 do arquivo		*/
+	while(fgetc(f))							/*	enquanto ainda é puxado bit valor 1 do arquivo		*/
 	{
 		printf("----------------------\nEstrutura %d\n",i);
 		one_struct(f);
@@ -78,3 +36,54 @@ int decode (FILE *f)
 
 	return 0;
 }
+/* Funcao 
+int fgetc(FILE *arq) retorna como int do primeiro byte do arquivo*/
+
+void one_field(FILE *f)
+{
+	int i, numss=0, exp=0, numcs;
+
+	for(i=0;i<6;i++)
+		c=fgetc(f);							/*passa os 5 bits 0 da chave */
+
+	if(fgetc(f))							/*confere se é long ou int e imprime */
+		printf("<long>");
+	else
+		printf("<int>");
+
+	while(fgetc(f))						/*confere se é ultimo byte */
+	{
+		for(i=0;i<BYTE-1;i++,exp++)
+		{
+			numss = numss + fgetc(f)*pow(2,exp);	/*implementar sabendo que inteiro tem que ser transformado com sinal */
+		}
+	}
+
+	for(i=0;i<BYTE-1;i++,exp++) 			/* Processa ultimo byte */
+	{
+		numss = numss + fgetc(f)*pow(2,exp);		/*implementar sabendo que inteiro tem que ser transformado com sinal */
+	}
+
+
+	numcs = numss >> 1;
+	if(numss & 0x01)
+		numcs = ~numcs;
+	
+	printf("%d\n",numcs);
+
+	return;
+}
+
+void one_struct(FILE *f)
+{
+	int i, c, exp=0, num=0;
+
+	for(i=0;i<BYTE-1;i++)
+		c=fgetc(f);								/* acaba de ler a marca FF da estrutura */
+
+	while(fgetc(f))							/* confere se é o ultimo campo */
+		one_field(f);
+
+	one_field(f);								/* processa ultimo campo */
+}
+
